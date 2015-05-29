207da40c37eb13e45465f933257643e112039dad
Danny Graham <good.midget@gmail.com>
2015-05-29 11:18:47 -0700

    debugging multipart uploads

--- test/index.js
+++ test/index.js
@@ -8,6 +8,7 @@ var request = require('request');
 var port = 9010;
 var server = 'http://localhost:' + port;
 
+// We'll use a public domain image for testing with
 // jscs:disable
 var stashSrc = 'http://upload.wikimedia.org/wikipedia/commons/9/9f/Cat_public_domain_dedication_image_0012.jpg'; // jshint ignore:line
 var stashKey = '2390423490';
@@ -15,6 +16,7 @@ var stashKey = '2390423490';
 
 var fixtures = {
   thumbs: require('./fixtures/thumbnail.json'),
+  operations: require('./fixtures/operations.json'),
 };
 
 var stashFile = {
@@ -54,72 +56,68 @@ describe('Stash-Magick REST Service', function() {
   });
 });
 
-// describe('Image Upload', function() {
-//   it('Can accept an uploaded image to stash', function(done) {
-//     this.timeout(30000);
-//     if (!fs.existsSync('./test/fixtures/sample.jpg')) {
-//       request(stashSrc, function(err, res) {
-//         assert(res.statusCode === 200);
-//         request.put(server + '/image/', stashFile, function(err, res) {
-//           assert(res.statusCode === 200);
-//           done();
-//         });
-//       })
-//       .pipe(fs.createWriteStream('./test/fixtures/sample.jpg'));
-//     } else {
-//       request.put(server + '/image/', stashFile, function(err, res) {
-//         assert(res.statusCode === 200);
-//         done();
-//       });
-//     }
-//   });
+describe('Image Upload', function() {
+  it('Can accept an uploaded image to stash', function(done) {
+    this.timeout(30000);
 
-//   it('Can accept an uploaded image to update the stash', function(done) {
-//     this.timeout(30000);
-//     request.put(server + '/image/' + stashKey, stashFile, function(err, res) {
-//       assert(res.statusCode === 200);
-//       done();
-//     });
-//   });
-// });
+    // In our first test here, let's make sure we
+    // have a sample image to work with
+    if (!fs.existsSync('./test/fixtures/sample.jpg')) {
+      request(stashSrc, function(err, res) {
+        assert(res.statusCode === 200);
+        request.put({
+          url: server + '/image/',
+          formData: stashFile,
+        }, function(err, res) {
+          assert(res.statusCode === 200);
+          done();
+        });
+      })
+      .pipe(fs.createWriteStream('./test/fixtures/sample.jpg'));
+    } else {
+      request.put({
+        url: server + '/image/',
+        formData: stashFile,
+      }, function(err, res) {
+        assert(res.statusCode === 200);
+        done();
+      });
+    }
+  });
 
-describe('Image Transforms', function() {
-
-  var formData = fixtures.thumbs;
-
-  it('Can create thumbnails from an upload stream', function(done) {
+  it.skip('Can update an image in the stash', function(done) {
     this.timeout(30000);
     request.put({
-      url: server + '/image/',
+      url: server + '/image/' + stashKey,
       formData: {
-        transform: JSON.stringify(fixtures.thumbs.transform),
-        attachments: [
-          fs.createReadStream('./test/fixtures/sample.jpg'),
-        ],
+        image: {
+          value:  fs.createReadStream('./test/fixtures/sample.jpg'),
+          options: {
+            filename: 'sample.jpg',
+            contentType: 'image/jpg',
+          },
+        },
       },
     }, function(err, res) {
       assert(res.statusCode === 200);
       done();
     });
   });
-  // it('Can create thumbnails from an upload stream', function(done) {
-  //   this.timeout(30000);
-  //   request({
-  //     method: 'PUT',
-  //     uri: server + '/image/',
-  //     preambleCRLF: true,
-  //     postambleCRLF: true,
-  //     multipart: [
-  //       {
-  //         'content-type': 'application/json',
-  //         body: JSON.stringify(fixtures.thumbs),
-  //       },
-  //       { body: 'I am an attachment' },
-  //       // { body: fs.createReadStream('./test/fixtures/sample.jpg') },
-  //     ],
-  //   }, function(err, res) {
-  //     assert(res.statusCode === 200);
-  //     done();
-  //   });
-  // });
+});
+
+describe('Image Transforms', function() {
+
+  var formData = fixtures.thumbs;
+
+  it.skip('Can create thumbnails from an upload stream', function(done) {
+    this.timeout(30000);
+    request.put({
+      url: server + '/image/',
+      formData: stashFile,
+    }, function(err, res) {
+      assert(res.statusCode === 200);
+      done();
+    });
+  });
+
 });
